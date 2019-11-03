aef0c0d59fece38d667cdeb13007473a7884a150
krulvis <joepkleint@gmail.com>
2019-11-03 22:50:45 +0100

    Dockerfile for Flask server

--- gan_server/server.py
+++ gan_server/server.py
@@ -32,21 +32,24 @@ vocab_size = input_y.shape.as_list()[1]
 
 initializer = tf.global_variables_initializer()
 
-sess = tf.Session()
+sess = tf.Session(config=tf.ConfigProto(gpu_options=tf.GPUOptions(allow_growth=True)))
 sess.run(initializer)
+
 
 def truncated_z_sample(batch_size):
     values = truncnorm.rvs(-2, 2, size=(batch_size, dim_z), random_state=random_state)
     return truncation * values
 
+
 def create_labels(num, max_classes):
     label = np.zeros((num, vocab_size))
     for i in range(len(label)):
         for _ in range(random.randint(1, max_classes)):
-            j = random.randint(0, vocab_size-1)
+            j = random.randint(0, vocab_size - 1)
             label[i, j] = random.random()
         label[i] /= label[i].sum()
     return label
+
 
 def sample(vectors, labels, batch_size=10):
     num = vectors.shape[0]
@@ -61,15 +64,16 @@ def sample(vectors, labels, batch_size=10):
     ims = np.uint8(ims)
     return ims
 
+
 def create_variations(num, vector, label):
     new_vectors = np.zeros((num, vector.shape[0]))
-    new_labels  = np.zeros((num, label.shape[0]))
+    new_labels = np.zeros((num, label.shape[0]))
 
     vector_mutation_rate = vector.std() * 4
 
     for i in range(num):
         new_labels[i][:] = label
-        dv = (np.random.rand(*vector.shape)-0.5) * vector_mutation_rate
+        dv = (np.random.rand(*vector.shape) - 0.5) * vector_mutation_rate
         new_vectors[i] = vector + dv
         new_vectors[i] /= max(-new_vectors.min(), new_vectors.max())
 
@@ -82,7 +86,7 @@ def create_variations(num, vector, label):
 
         # Add class.
         if random.random() < 0.3:
-            new_labels[i][random.randint(0, label.shape[0]-1)] += random.random() * 0.5
+            new_labels[i][random.randint(0, label.shape[0] - 1)] += random.random() * 0.5
 
         # Remove if less than two percent.
         new_labels[new_labels < .02] = 0
@@ -92,16 +96,18 @@ def create_variations(num, vector, label):
 
     return new_vectors, new_labels
 
+
 def interpolate(num, vector1, vector2, label1, label2):
-    x = np.linspace(0, 1, num+2)
+    x = np.linspace(0, 1, num + 2)
     new_vectors = np.zeros((num, vector1.shape[0]))
-    new_labels  = np.zeros((num, label1.shape[0]))
+    new_labels = np.zeros((num, label1.shape[0]))
 
     for i, v in enumerate(x[1: -1]):
-        new_labels[i]  = v*label1 + (1-v) * label2
-        new_vectors[i] = v*vector1 + (1-v) * vector2
+        new_labels[i] = v * label1 + (1 - v) * label2
+        new_vectors[i] = v * vector1 + (1 - v) * vector2
 
     return new_vectors, new_labels
+
 
 def create_random_images(num_images, max_classes):
     vectors = truncated_z_sample(num_images)
@@ -110,21 +116,25 @@ def create_random_images(num_images, max_classes):
 
     return ims, vectors, labels
 
+
 def encode_img(arr):
     # Encode uint8 values array into base64 string for sending.
     image = PIL.Image.fromarray(arr)
     buffered = BytesIO()
     image.save(buffered, format="JPEG", quality=90)
     img_bytes = base64.b64encode(buffered.getvalue())
-    img_str = 'data:image/jpeg;base64,'+img_bytes.decode('ascii')
+    img_str = 'data:image/jpeg;base64,' + img_bytes.decode('ascii')
     return img_str
 
-app = Flask(__name__, static_url_path='') #, static_folder='public', )
+
+app = Flask(__name__, static_url_path='')  # , static_folder='public', )
 CORS(app)
+
 
 @app.route('/')
 def index():
     return "Hello, World!"
+
 
 @app.route('/random', methods=['POST'])
 def get_random():
@@ -133,9 +143,9 @@ def get_random():
         print('Random', num)
         t = time.time()
         imgs, vectors, labels = create_random_images(num, max_classes=3)
-        print('Finished in', time.time()-t)
+        print('Finished in', time.time() - t)
         response = jsonify([
-            [ encode_img(arr) for arr in imgs ],
+            [encode_img(arr) for arr in imgs],
             vectors.tolist(),
             labels.tolist()
         ])
@@ -144,26 +154,28 @@ def get_random():
         print(e)
         return '', 500
 
+
 @app.route('/children', methods=['POST'])
 def children():
     try:
         print('children')
         vector = json.loads(request.form['vector'])
-        label  = json.loads(request.form['label'])
+        label = json.loads(request.form['label'])
         vector = np.asarray(vector, dtype='float64')
-        label  = np.asarray(label, dtype='float64')
+        label = np.asarray(label, dtype='float64')
         new_vectors, new_labels = create_variations(12, vector, label)
 
         new_ims = sample(new_vectors, new_labels)
 
         return jsonify([
-            [ encode_img(arr) for arr in new_ims ],
+            [encode_img(arr) for arr in new_ims],
             new_vectors.tolist(),
             new_labels.tolist()
         ])
     except Exception as e:
         print(e)
         return '', 500
+
 
 @app.route('/mix_images', methods=['POST'])
 def mix_images():
@@ -175,13 +187,14 @@ def mix_images():
         new_vectors, new_labels = interpolate(12, vector1, vector2, label1, label2)
         new_ims = sample(new_vectors, new_labels)
         return jsonify([
-            [ encode_img(arr) for arr in new_ims ],
+            [encode_img(arr) for arr in new_ims],
             new_vectors.tolist(),
             new_labels.tolist()
         ])
     except Exception as e:
         print(e)
         return '', 500
+
 
 if __name__ == '__main__':
     port = int(sys.argv[1]) if len(sys.argv) > 1 else 5000
